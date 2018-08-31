b94fb355a72fc9c4bb10286160d130b40ef71e2d
Kristen Shea <kstar61@live.com>
2018-08-30 22:44:51 -0500

    program that prompts user for radius of a circle and calculates the area using a method. program will return invalid and prompt user for a new radius if user enters a negative number using a do-while loop.

--- radius/Program.cs
+++ radius/Program.cs
@@ -6,7 +6,40 @@ namespace radius
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //•	Write a program that prompts the user for a number representing the radius, 
+            //and then calculates the area of a circle with that radius and prints the result.
+            //Extend your program further by using a while or do -while loop, so that when the 
+            //user enters a negative number they are re-prompted.
+
+            Console.WriteLine("Calculate the area of a circle.");
+            Console.Write("Please enter the radius: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            do
+            {
+                
+                Console.Write("Invalid entry. \n Enter radius: ");
+                
+                radius = int.Parse(Console.ReadLine());
+            }
+            while (radius < 0);
+           
+
+            Console.WriteLine($"Area of circle: {CircleArea(radius)}");
+
+            Console.ReadLine();
+
         }
+
+
+        private static double CircleArea(int radius)
+        {
+
+            double radiusSqu = Math.Pow(radius, 2);
+            return radiusSqu * Math.PI;
+
+        }
+
+
     }
 }
